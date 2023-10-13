acd16e650459c38cc52d3658dfa8d8a9b63788b2
Animeshmitra08 <animeshmitra003@gmail.com>
2023-10-13 19:28:36 +0530

    made some changes in webkit scrollbar, landing page and added animations

--- src/Pages/Landing_Page.js
+++ src/Pages/Landing_Page.js
@@ -1,23 +1,30 @@
-import React from 'react'
-import Cards from '../Components/Cards'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { scroll } from "framer-motion";
 
 const Landing_Page = () => {
   return (
     <>
-      <main className="relative mx-auto -mt-[5.75rem] overflow-hidden pb-16 pt-[5.75rem]">
+      <motion.main className="relative mx-auto -mt-[5.75rem] overflow-hidden pb-16 pt-[5.75rem]">
         <img src="./assets/v904-nunny-012.jpg" className="absolute h-screen -top-[1rem] -ml-[120rem] max-w-none w-[166rem] -z-10 sm:-ml-[100.5rem] left-1/2"/>
         <section className="relative w-full max-w-container px-4 sm:px-6 lg:px-8 h-[35rem] sm:h-[28em] mt-[10rem] flex flex-col items-center">
-          <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+          <motion.h1 
+          initial = {{y:50, opacity:0}}
+          animate={{y:0, opacity:1}}
+            transition={{ ease: "easeIn", duration: 2 }}
+          className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
             Make Yourself More Beautiful
-          </h1>
+          </motion.h1>
           <h2 className="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus totam deserunt dolorem voluptatem distinctio facilis molestiae, labore eum ipsum iste.
           </h2>
           <br />
           <div>
-            <button className="border-2 py-3 bg-black text-slate-50 rounded-xl w-[10rem] shadow-lg shadow-slate-500 border-none mb-2">
+            <motion.button 
+            whileHover={{scale:1.05}}
+            className="border-2 py-3 bg-black text-slate-50 rounded-xl w-[10rem] shadow-lg shadow-slate-500 border-none mb-2">
               Get Started
-            </button>
+            </motion.button>
           </div>
         </section>
         <section className="relative w-full max-w-container bg-[rgb(238,247,248)]">
@@ -28,7 +35,7 @@ const Landing_Page = () => {
            We have differnt types of Template
           </div>
         </section>
-      </main>
+      </motion.main>
       
     </>
   )
