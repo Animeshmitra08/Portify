a5216fb7154d6ae32ec09cefacdc9eda976fc3e5
Animeshmitra08 <animeshmitra003@gmail.com>
2023-12-29 10:21:30 +0530

    menues in settings and profile

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,16 +1,24 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { HiMiniBars3BottomLeft } from "react-icons/hi2";
 import { MdSettings } from "react-icons/md";
+import SettingsC from '../Components/SettingsC';
 
 const Dashboard = () => {
 
   const [avatarDropdown, setAvatarDropdown] = useState(false);
+  const [settingsOpen, setSettingsOpen] = useState(false);
   
+  //for avatar----------
   const handleclick = () =>{
     setAvatarDropdown((prev)=> !prev);
   }
+  //for settings-----------------
+  const settingsClick =()=>{
+    setSettingsOpen(!settingsOpen);
+  }
   
-  let divref = useRef();
+  let divref = useRef();                                //to refer the elements of profile avatar                    
+  let settingsref = useRef();                           //to refer the elements of settings
 
   useEffect(() => {
     const mouseHandler =(e)=>{
@@ -18,10 +26,19 @@ const Dashboard = () => {
         setAvatarDropdown(false);
       }      
     }
+
+    const mouseHandler2 = (e)=>{
+      if (!settingsref.current.contains(e.target)) {
+        setSettingsOpen(false)
+      }
+    }
+
     document.addEventListener('mousedown', mouseHandler);
+    document.addEventListener('mousedown', mouseHandler2);
 
     return () => {
       document.removeEventListener('mousedown', mouseHandler);
+      document.removeEventListener('mousedown', mouseHandler2);
     }
   },[])
   
@@ -31,26 +48,38 @@ const Dashboard = () => {
   return (
     <>
       <div className="bg-slate-800 w-100 h-screen overflow-hidden">
-        <h1 className="absolute text-gray-50 left-10 top-1 text-lg">Dashboard</h1>
-        <HiMiniBars3BottomLeft className="w-8 h-8 absolute fill-slate-400 left-1 top-8 cursor-pointer"/>
-        <MdSettings className="w-8 h-8 absolute bottom-2 left-1 fill-slate-400 cursor-pointer"/>
+        <h1 className="absolute text-gray-50 left-12 top-1 text-lg">Dashboard</h1>
 
-        <div ref={divref}>
-           <button className="bg-slate-50 cursor-pointer absolute right-2 top-1 flex items-center px-2 gap-1 rounded-full" onClick={handleclick}>
-          <span>Animesh</span>
-          <img className="w-8 h-8 rounded-full " src="/assets/avatar1.jpg" alt="Medium avatar"/>
+        <button className="absolute left-1 top-1" >
+          <HiMiniBars3BottomLeft className="w-8 h-8 fill-slate-400"/>
         </button>
+
+        {/* settings------------------- */}
+        <div ref={settingsref}>
+          <button className="absolute bottom-2 left-1" onClick={settingsClick}>
+            <MdSettings className="w-8 h-8 fill-slate-400" />
+          </button>
+          {settingsOpen && <SettingsC/>}
+        </div>
         
-        {avatarDropdown && 
-        <div className="bg-slate-50 w-[150px] rounded-md p-2 absolute shadow-lg right-2 top-[50px] drpdwn">
-          <ul className="flex flex-col gap-1">
-            <li>Profile</li>
-            <hr/>
-            <li>Settings</li>
-            <hr/>
-            <li>Logout</li>
-          </ul>
-        </div>}
+
+        {/* profile avatar-------------------------- */}
+        <div ref={divref}>
+          <button className="bg-slate-50 cursor-pointer absolute right-2 top-1 flex items-center px-2 gap-1 rounded-full" onClick={handleclick}>
+            <span>Animesh</span>
+            <img className="w-8 h-8 rounded-full " src="/assets/avatar1.jpg" alt="Medium avatar"/>
+          </button>
+        
+          {avatarDropdown && 
+          <div className="bg-slate-50 w-[150px] rounded-md p-2 absolute shadow-lg right-2 top-[50px] drpdwn border-gray-500 border-[1px]">
+            <ul className="flex flex-col gap-1">
+              <li>Profile</li>
+              <hr/>
+              <li>Settings</li>
+              <hr/>
+              <li>Logout</li>
+            </ul>
+          </div>}
         </div>
        
         
